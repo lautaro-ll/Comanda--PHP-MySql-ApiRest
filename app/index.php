85767e49a009ebddd681e4f78ad949c6cbeaa88f
Lautaro LL <62777637+lautaro-ll@users.noreply.github.com>
2021-05-16 21:19:38 -0300

    first commit

---
+++ app/index.php
@@ -0,0 +1,91 @@
+<?php
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+use Slim\Factory\AppFactory;
+use Slim\Psr7\Response;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->add(function (Request $request, RequestHandler $handler) {
+    $response = $handler->handle($request);
+    $response->getBody()->write(' BEFFORRRRE');
+    $existingContent = (string) $response->getBody();
+    $response = new Response();
+    $response->getBody()->write('BEFORE ' . $existingContent);
+
+    return $response;
+});
+
+$app->add(function (Request $request, RequestHandler $handler) {
+    $response = $handler->handle($request);
+    $response->getBody()->write(' AFTER');
+    return $response;
+});
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write('Hello World');
+    return $response;
+});
+
+$app->run();
+
+
+
+/*
+error_reporting(-1);
+ini_set('display_errors', 1);
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+use Slim\Factory\AppFactory;
+use Slim\Routing\RouteCollectorProxy;
+use Slim\Routing\RouteContext;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+require_once './db/AccesoDatos.php';
+// require_once './middlewares/Logger.php';
+
+require_once './controllers/UsuarioController.php';
+
+// Instantiate App
+$app = AppFactory::create();
+
+// Add error middleware
+$app->addErrorMiddleware(true, true, true);
+
+$mw = function (Request $request, RequestHandler $handler) {
+  $response = $handler->handle($request);
+  $response->getBody()->write('World');
+
+  return $response;
+};
+
+//VERSION < 4
+$mw = function ($request, $response, $next) {
+  $response->getBody()->write('BEFORE');
+  $response = $next($request, $response);
+  $response->getBody()->write('AFTER');
+
+  return $response;
+};
+
+// Routes
+$app->group('/usuarios', function (RouteCollectorProxy $group) {
+    $group->get('[/]', \UsuarioController::class . ':TraerTodos');
+    $group->get('/{usuario}', \UsuarioController::class . ':TraerUno');
+    $group->post('[/]', \UsuarioController::class . ':CargarUno');
+  });
+
+$app->get('[/]', function (Request $request, Response $response) {    
+    $response->getBody()->write("Slim Framework 4 PHP");
+    return $response;
+
+})->add($mw);
+
+$app->run();
+*/
