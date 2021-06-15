79cd109b7a7fc752fec3863bee6d36e11cf78cd2
Lautaro LL <62777637+lautaro-ll@users.noreply.github.com>
2021-06-15 02:08:09 -0300

    commit

--- app/controllers/PedidoController.php
+++ app/controllers/PedidoController.php
@@ -468,7 +468,7 @@ public function FueraDeTiempo($request, $response, $args)
 
       $lista = Pedido::join('productos', 'pedidos.producto_id', '=', 'productos.id')
               ->whereBetween('tiempo_pedido', [$desde, $hasta])
-              ->where('tiempo_estimado', '>', 'tiempo_finalizado')
+              ->where('pedidos.tiempo_estimado', '>', 'pedidos.tiempo_finalizado')
               ->get();
 
         $payload = json_encode(array("Lista:" => $lista));
